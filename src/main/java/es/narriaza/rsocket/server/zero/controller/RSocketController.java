1ba763de797b0758fb352d21cd0c7fba54eaea2a
Nicolás Arriaza <tiffydus@users.noreply.github.com>
2020-09-23 03:08:09 +0200

    Add rsocket message handlers (#1)
    Co-authored-by: Niko <narriaza@paradigmadigital.com>

--- src/main/java/es/narriaza/rsocket/server/zero/controller/RSocketController.java
+++ src/main/java/es/narriaza/rsocket/server/zero/controller/RSocketController.java
@@ -5,6 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.stream.Stream;
 
 @Controller
 public class RSocketController {
@@ -15,6 +20,40 @@ public class RSocketController {
     Message requestResponse(Message request){
         LOGGER.info("Received request-response message {}", request);
         return new Message("nikoServer","ResponseFromRequest");
+    }
+
+    @MessageMapping("request-response-flux")
+    Mono<Message> requestResponseFlux(Mono<Message> request){
+        LOGGER.info("Received request-response message {}", request);
+        return Mono.just(new Message("nikoServer","ResponseFromRequest"));
+    }
+
+    @MessageMapping("fire-and-forget")
+    Mono<Void> fireAndForget(Mono<Message> request){
+        LOGGER.info("Received fire and forget message {}", request);
+        // TODO do something
+        return Mono.empty();
+    }
+
+    @MessageMapping("request-stream")
+    public Flux<Message> requestStream(Message request){
+        LOGGER.info("Received request stream message {}", request);
+        // TODO do something
+        var stream = Stream.generate(() -> new Message(
+                "nikoServer"+request.getOrigin(),
+                "Response: "+request.getInteraction()
+        ));
+
+        return Flux.fromStream(stream).checkpoint("Stream added")
+                .delayElements(Duration.ofSeconds(1))
+                .doOnNext(m -> LOGGER.debug("Message: {}", m));
+    }
+
+    @MessageMapping("channel")
+    Flux<Message> channel(Flux<Message> request){
+        LOGGER.info("Received channel message {}", request);
+        // TODO do something
+        return Flux.empty();
     }
 
 }
